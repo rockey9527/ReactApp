b9faab515511e770e40682cd34b855c937fb49db
周鹏程 <1123844553@qq.com>
2024-05-17 18:52:01 +0800

    init-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from "react";
 import Login from './pages/login'
 import Home from './pages/home'
+import City from './pages/city'
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route path="/login" component={Login}/>
           <Route path="/home" component={Home}/>
+          <Route path="/city" component={City}/>
           <Route exat path="/" component={Home} />
           <Redirect to={'/home'}></Redirect>
         </Switch>
---
+++ src/pages/city/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './city.css'
+
+class City extends Component {
+    render(){
+        return (
+            <div>
+                <h1 className="P-City">
+                    This is City page
+                </h1>
+                <br />
+                
+            </div>
+        )
+    }
+}
+ export default City
